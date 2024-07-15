2580898336548ad375f59b7451a2844fd8b680c6
Lonnickr <lonnickr@gmail.com>
2024-07-15 15:41:52 -0400

    update2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 console.log("Tis-Tac-Toe!")
 
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCoombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 8],
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -17,10 +25,10 @@ const messageEl = document.getElementById('message')
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
-    board = ['', 'X', '', 'O', '', '', '', '', '',]
+    board = ['', '', '', '', '', '', '', '', '',]
     turn = 'X'
-    winner = true
-    tie = true
+    winner = false
+    tie = false
     render()
 }
 
@@ -51,7 +59,61 @@ function updateMessage() {
     }
 }
 
-/*----------------------------- Event Listeners -----------------------------*/
+function handleClick(evt) {
+    const squareIdx = parseInt(evt.target.id)
+    if (board[squareIdx] === "X" || board[squareIdx] === "O" || winner) {
+        return 
+    }
+    placePiece(squareIdx)
+    checkForwinner()
+    checkforTie()
+    switchPlayerTurn()
+    render()
+}
 
+function placePiece(idx) {
+    board[idx] = turn
+    console.log(board);
+}
+
+function checkForwinner() {
+    if (
+       (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) ||
+       (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) ||
+       (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) ||
+       (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||
+       (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||
+       (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||
+       (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||
+       (board[2] !== '' && board[2] === board[4] && board[2] === board[6])
+      )  {
+        winner = true
+    }
+}
+
+function checkforTie() {
+    if (winner) {
+        return
+    }
+    if (!board.includes('')) {
+        tie = true
+    }
+}
+
+function switchPlayerTurn() {
+    if (winner) {
+        return
+    }
+    if (turn === 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+squareEl.forEach((squareEl) => {
+    squareEl.addEventListener("click", handleClick)
+})
 
 
